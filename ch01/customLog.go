728be1ea24c5956bbc575583e8adba808c92e7d2
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2019-01-10 21:52:32 +0200

    Tabs to spaces

--- ch01/customLog.go
+++ ch01/customLog.go
@@ -1,25 +1,25 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"os"
+    "fmt"
+    "log"
+    "os"
 )
 
 var LOGFILE = "/tmp/mGo.log"
 
 func main() {
-	f, err := os.OpenFile(LOGFILE, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+    f, err := os.OpenFile(LOGFILE, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer f.Close()
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+    defer f.Close()
 
-	// LstdFlags = Ldate | Ltime
-	// initial values for the standard logger
-	iLog := log.New(f, "customLog ", log.LstdFlags)
-	iLog.Println("Hello there!")
-	iLog.Println("Another log entry!")
+    // LstdFlags = Ldate | Ltime
+    // initial values for the standard logger
+    iLog := log.New(f, "customLog ", log.LstdFlags)
+    iLog.Println("Hello there!")
+    iLog.Println("Another log entry!")
 }
--- ch01/customLogLineNumber.go
+++ ch01/customLogLineNumber.go
@@ -1,28 +1,28 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"os"
+    "fmt"
+    "log"
+    "os"
 )
 
 var LOGFILE = "/tmp/mGo.log"
 
 func main() {
-	f, err := os.OpenFile(LOGFILE, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+    f, err := os.OpenFile(LOGFILE, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer f.Close()
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+    defer f.Close()
 
-	// LstdFlags = Ldate | Ltime
-	// initial values for the standard logger
-	iLog := log.New(f, "customLogLineNumber ", log.LstdFlags)
+    // LstdFlags = Ldate | Ltime
+    // initial values for the standard logger
+    iLog := log.New(f, "customLogLineNumber ", log.LstdFlags)
 
-	// Print the line number of when and where something has happened
-	iLog.SetFlags(log.LstdFlags | log.Lshortfile)
-	iLog.Println("Hello there!")
-	iLog.Println("Another log entry!")
+    // Print the line number of when and where something has happened
+    iLog.SetFlags(log.LstdFlags | log.Lshortfile)
+    iLog.Println("Hello there!")
+    iLog.Println("Another log entry!")
 }
