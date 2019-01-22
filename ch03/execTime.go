183f57f239fe0073fbe776863ca544fec23fd246
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2019-01-22 13:46:35 +0200

    Find duration of command

--- ch03/execTime.go
+++ ch03/execTime.go
@@ -2,9 +2,26 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 	"time"
 )
 
-func main() {
+type data struct {
+	i, j int
+}
 
+func main() {
+	var N = 40000000
+	var structure []data
+	for i := 0; i < N; i++ {
+		value := int(i)
+		structure = append(structure, data{value, value})
+	}
+
+	start := time.Now()
+	runtime.GC()
+	duration := time.Since(start)
+
+	fmt.Println("It took GC()", duration, "to finish")
+	_ = structure[0]
 }
