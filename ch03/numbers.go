e79501c550bb9b98a5bb3472da0abef63442cf78
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2019-01-24 20:21:37 +0200

    Working with numbers in Go

--- ch03/numbers.go
+++ ch03/numbers.go
@@ -5,5 +5,34 @@ import (
 )
 
 func main() {
+	c1 := 12 + 1i
+	c2 := complex(5, 7)
+	fmt.Printf("Type of c1: %T\n", c1)
+	fmt.Printf("Type of c2: %T\n", c2)
 
+	var c3 complex64 = complex64(c1 + c2)
+	fmt.Println("c3:", c3)
+	fmt.Printf("Type of c3: %T\n", c3)
+
+	cZero := c3 - c3
+	fmt.Println("cZero:", cZero)
+
+	x := 12
+	k := 5
+	fmt.Println(x)
+	fmt.Printf("Type of x: %T\n", x)
+
+	div := x / k
+	fmt.Println("div", div)
+
+	var m, n float64
+	m = 1.223
+	fmt.Println("m, n:", m, n)
+
+	y := 4 / 2.3
+	fmt.Println("y:", y)
+
+	divFloat := float64(x / k)
+	fmt.Println("divFloat", divFloat)
+	fmt.Printf("Type of divFloat: %T\n", divFloat)
 }
