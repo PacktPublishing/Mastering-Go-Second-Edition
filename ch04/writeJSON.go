7d82891e51f0ccb783f5a2c1ff1474567fd64a99
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2019-01-31 20:41:12 +0200

    Write to os.Stdout

--- ch04/writeJSON.go
+++ ch04/writeJSON.go
@@ -6,6 +6,34 @@ import (
 	"os"
 )
 
-func main() {
+type Record struct {
+	Name    string
+	Surname string
+	Tel     []Telephone
+}
 
+type Telephone struct {
+	Mobile bool
+	Number string
+}
+
+func saveToJSON(filename *os.File, key interface{}) {
+	encodeJSON := json.NewEncoder(filename)
+	err := encodeJSON.Encode(key)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+
+func main() {
+	myRecord := Record{
+		Name:    "Mihalis",
+		Surname: "Tsoukalos",
+		Tel: []Telephone{Telephone{Mobile: true, Number: "1234-567"},
+			Telephone{Mobile: true, Number: "1234-abcd"},
+			Telephone{Mobile: false, Number: "abcc-567"},
+		}}
+
+	saveToJSON(os.Stdout, myRecord)
 }
