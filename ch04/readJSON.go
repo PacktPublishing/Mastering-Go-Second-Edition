41c782afe7ea8d7439d3ea45d031ac798dd47c15
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2019-02-01 12:27:20 +0200

    Sample JSON record

--- ch04/readJSON.go
+++ ch04/readJSON.go
@@ -6,6 +6,46 @@ import (
 	"os"
 )
 
-func main() {
+type Record struct {
+	Name    string
+	Surname string
+	Tel     []Telephone
+}
 
+type Telephone struct {
+	Mobile bool
+	Number string
+}
+
+func loadFromJSON(filename string, key interface{}) error {
+	in, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+
+	decodeJSON := json.NewDecoder(in)
+	err = decodeJSON.Decode(key)
+	if err != nil {
+		return err
+	}
+	in.Close()
+	return nil
+}
+
+func main() {
+	arguments := os.Args
+	if len(arguments) == 1 {
+		fmt.Println("Please provide a filename!")
+		return
+	}
+
+	filename := arguments[1]
+
+	var myRecord Record
+	err := loadFromJSON(filename, &myRecord)
+	if err == nil {
+		fmt.Println(myRecord)
+	} else {
+		fmt.Println(err)
+	}
 }
