e5889250bd0d7d22788b18a3960ba745929b1a38
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2019-02-02 14:01:06 +0200

    XML related utilities

--- ch04/rwXML.go
+++ ch04/rwXML.go
@@ -7,6 +7,68 @@ import (
 	"os"
 )
 
-func main() {
+type Record struct {
+	Name    string
+	Surname string
+	Tel     []Telephone
+}
 
+type Telephone struct {
+	Mobile bool
+	Number string
+}
+
+func loadFromJSON(filename string, key interface{}) error {
+	in, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+
+	decodeJSON := json.NewDecoder(in)
+	err = decodeJSON.Decode(key)
+	if err != nil {
+		return err
+	}
+	in.Close()
+	return nil
+}
+
+func main() {
+	arguments := os.Args
+	if len(arguments) == 1 {
+		fmt.Println("Please provide a filename!")
+		return
+	}
+
+	filename := arguments[1]
+
+	var myRecord Record
+	err := loadFromJSON(filename, &myRecord)
+	if err == nil {
+		fmt.Println("JSON:", myRecord)
+	} else {
+		fmt.Println(err)
+	}
+
+	myRecord.Name = "Dimitris"
+
+	xmlData, _ := xml.MarshalIndent(myRecord, "", "    ")
+	xmlData = []byte(xml.Header + string(xmlData))
+	fmt.Println("xmlData:", string(xmlData))
+
+	data := &Record{}
+	err = xml.Unmarshal(xmlData, data)
+	if nil != err {
+		fmt.Println("Unmarshalling from XML", err)
+		return
+	}
+
+	result, err := json.Marshal(data)
+	if nil != err {
+		fmt.Println("Error marshalling to JSON", err)
+		return
+	}
+
+	_ = json.Unmarshal([]byte(result), &myRecord)
+	fmt.Println("JSON:", myRecord)
 }
