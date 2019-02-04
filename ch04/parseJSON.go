fda750ea495be75afbd36eb82fad923da0fe63aa
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2019-02-04 21:32:36 +0200

    Parsing Unstructured JSON data file

--- ch04/parseJSON.go
+++ ch04/parseJSON.go
@@ -3,9 +3,29 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"os"
 )
 
 func main() {
+	arguments := os.Args
+	if len(arguments) == 1 {
+		fmt.Println("Please provide a filename!")
+		return
+	}
 
+	filename := arguments[1]
+
+	fileData, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	var parsedData map[string]interface{}
+	json.Unmarshal([]byte(fileData), &parsedData)
+
+	for key, value := range parsedData {
+		fmt.Println("key:", key, "value:", value)
+	}
 }
