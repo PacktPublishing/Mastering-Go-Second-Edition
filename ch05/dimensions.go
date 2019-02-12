de59d94e3b3d42003602e20d87102a16eeca442f
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2019-02-12 12:31:56 +0200

    Array dimensions

--- ch05/dimensions.go
+++ ch05/dimensions.go
@@ -5,8 +5,10 @@ import (
 )
 
 func main() {
-	foo := [2][4][7]float64{}
-	x := len(foo)       // x == 2
-	y := len(foo[0])    // y == 4
-	z := len(foo[0][0]) // z == 8
+	array := [12][4][7][10]float64{}
+	x := len(array)
+	y := len(array[0])
+	z := len(array[0][0])
+	w := len(array[0][0][0])
+	fmt.Println("x:", x, "y:", y, "z:", z, "w:", w)
 }
