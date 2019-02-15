2b683eb951e4168ac36b0ea5a19eecc72cbe4cb7
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2019-02-15 09:05:45 +0200

    Finished implementing variadic.go

--- ch06/variadic.go
+++ ch06/variadic.go
@@ -2,18 +2,30 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
-func varFunc() {
-
+func varFunc(input ...string) {
+	fmt.Println(input)
 }
 
-func oneByOne() int {
+func oneByOne(message string, s ...int) int {
+	fmt.Println(message)
 	sum := 0
-
+	for i, a := range s {
+		fmt.Println(i, a)
+		sum = sum + a
+	}
+	s[0] = -1000
 	return sum
 }
 
 func main() {
-
+	arguments := os.Args
+	varFunc(arguments...)
+	sum := oneByOne("Adding numbers...", 1, 2, 3, 4, 5, -1, 10)
+	fmt.Println("Sum:", sum)
+	s := []int{1, 2, 3}
+	sum = oneByOne("Adding numbers...", s...)
+	fmt.Println(s)
 }
