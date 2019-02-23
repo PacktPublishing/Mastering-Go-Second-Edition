a2e4278286bf277fff1d0b765f11c6354df03541
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2019-02-23 22:27:31 +0200

    Initial versions of the 2 examples

--- ch06/varTimes.go
+++ ch06/varTimes.go
@@ -2,9 +2,39 @@ package main
 
 import (
 	"fmt"
+	"go/ast"
+	"go/parser"
 	"go/token"
+	"os"
+	"strings"
 )
 
-func main() {
+type visitor int
 
+func (v visitor) Visit(n ast.Node) ast.Visitor {
+	if n == nil {
+		return nil
+	}
+
+	fmt.Printf("%s%T\n", strings.Repeat("\t", int(v)), n)
+	return v + 1
+}
+
+func main() {
+	if len(os.Args) == 1 {
+		fmt.Println("Not enough arguments!")
+		return
+	}
+
+	for _, file := range os.Args[1:] {
+		fmt.Println("Processing:", file)
+		one := token.NewFileSet()
+		var v visitor
+		f, err := parser.ParseFile(one, file, nil, parser.AllErrors)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		ast.Walk(v, f)
+	}
 }
