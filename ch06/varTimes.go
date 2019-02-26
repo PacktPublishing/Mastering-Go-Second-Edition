d459e57495601e8e26d8e67d694bae903076b71b
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2019-02-26 08:58:11 +0200

    Counting var in .go files

--- ch06/varTimes.go
+++ ch06/varTimes.go
@@ -2,23 +2,14 @@ package main
 
 import (
 	"fmt"
-	"go/ast"
-	"go/parser"
+	"go/scanner"
 	"go/token"
+	"io/ioutil"
 	"os"
-	"strings"
 )
 
-type visitor int
-
-func (v visitor) Visit(n ast.Node) ast.Visitor {
-	if n == nil {
-		return nil
-	}
-
-	fmt.Printf("%s%T\n", strings.Repeat("\t", int(v)), n)
-	return v + 1
-}
+var KEYWORD = "var"
+var COUNT = 0
 
 func main() {
 	if len(os.Args) == 1 {
@@ -28,13 +19,30 @@ func main() {
 
 	for _, file := range os.Args[1:] {
 		fmt.Println("Processing:", file)
-		one := token.NewFileSet()
-		var v visitor
-		f, err := parser.ParseFile(one, file, nil, parser.AllErrors)
+		f, err := ioutil.ReadFile(file)
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
-		ast.Walk(v, f)
+		one := token.NewFileSet()
+		files := one.AddFile(file, one.Base(), len(f))
+
+		var myScanner scanner.Scanner
+		myScanner.Init(files, f, nil, scanner.ScanComments)
+
+		localCount := 0
+		for {
+			_, tok, lit := myScanner.Scan()
+			if tok == token.EOF {
+				break
+			}
+
+			if lit == KEYWORD {
+				COUNT++
+				localCount++
+			}
+		}
+		fmt.Printf("Found _%s_ %d times\n", KEYWORD, localCount)
 	}
+	fmt.Printf("Found _%s_ %d times in total\n", KEYWORD, COUNT)
 }
