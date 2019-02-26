e18acc91e7c000fc4c9ffbcdf3eddd1a6dd46961
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2019-02-26 10:10:05 +0200

    Minor changes

--- ch06/varTwo.go
+++ ch06/varTwo.go
@@ -30,11 +30,12 @@ func main() {
 		return
 	}
 
+	all := token.NewFileSet()
+
 	for _, file := range os.Args[1:] {
 		fmt.Println("Processing:", file)
-		one := token.NewFileSet()
 		var v visitor
-		f, err := parser.ParseFile(one, file, nil, parser.AllErrors)
+		f, err := parser.ParseFile(all, file, nil, parser.AllErrors)
 		if err != nil {
 			fmt.Println(err)
 			return
