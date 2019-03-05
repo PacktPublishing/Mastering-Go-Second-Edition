97ffe43ba808d36141edbe7dad6ac0cae3e8753f
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2019-03-05 11:06:00 +0200

    Trying to use reflectwalk

--- ch07/walkRef.go
+++ ch07/walkRef.go
@@ -2,8 +2,64 @@ package main
 
 import (
 	"fmt"
+	"github.com/mitchellh/mapstructure"
+	"github.com/mitchellh/reflectwalk"
+	"reflect"
 )
 
-func main() {
+type Values struct {
+	Extra map[string]string
+}
 
+type WalkMap struct {
+	MapVal reflect.Value
+	Keys   map[string]bool
+	Values map[string]bool
+}
+
+func (t *WalkMap) Map(m reflect.Value) error {
+	t.MapVal = m
+	return nil
+}
+
+func (t *WalkMap) MapElem(m, k, v reflect.Value) error {
+	if t.Keys == nil {
+		t.Keys = make(map[string]bool)
+		t.Values = make(map[string]bool)
+	}
+
+	t.Keys[k.Interface().(string)] = true
+	t.Values[v.Interface().(string)] = true
+	return nil
+}
+
+func main() {
+	w := new(WalkMap)
+
+	type S struct {
+		Map map[string]string
+	}
+
+	data := &S{
+		Map: map[string]string{
+			"v1": "1v",
+			"v2": "2v",
+			"v3": "3v",
+			"v4": "4v",
+		},
+	}
+
+	err := reflectwalk.Walk(data, w)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(w.MapVal)
+
+	var result Values
+	if err := mapstructure.Decode(w.MapVal, &result); err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%+v", result)
 }
