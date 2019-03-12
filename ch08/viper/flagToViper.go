244527857d4e24b57cc7d9034c31f4c8aa88a65b
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2019-03-12 20:57:07 +0200

    How to use viper

--- ch08/viper/flagToViper.go
+++ ch08/viper/flagToViper.go
@@ -1,9 +1,31 @@
+// package main
+//
+// import (
+// 	"flag"
+// 	"fmt"
+// )
+//
+// func main() {
+// 	minusI := flag.Int("i", 100, "i parameter")
+// 	flag.Parse()
+// 	i := *minusI
+// 	fmt.Println(i)
+// }
+
 package main
 
 import (
+	"flag"
 	"fmt"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
 )
 
 func main() {
-
+	flag.Int("i", 100, "i parameter")
+	pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
+	pflag.Parse()
+	viper.BindPFlags(pflag.CommandLine)
+	i := viper.GetInt("i")
+	fmt.Println(i)
 }
--- ch08/viper/useViper.go
+++ ch08/viper/useViper.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+	"github.com/spf13/viper"
 )
 
 func main() {
-
+	viper.BindEnv("GOMAXPROCS")
+	val := viper.Get("GOMAXPROCS")
+	fmt.Println(val)
+	viper.Set("GOMAXPROCS", 10)
+	val = viper.Get("GOMAXPROCS")
+	fmt.Println(val)
 }
