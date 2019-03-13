6963e2e1cf4ad8b9797381a503809d6b0c82aab6
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2019-03-13 12:48:21 +0200

    Viper + environment variables

--- ch08/viper/useViper.go
+++ ch08/viper/useViper.go
@@ -8,8 +8,16 @@ import (
 func main() {
 	viper.BindEnv("GOMAXPROCS")
 	val := viper.Get("GOMAXPROCS")
-	fmt.Println(val)
+	fmt.Println("GOMAXPROCS:", val)
 	viper.Set("GOMAXPROCS", 10)
 	val = viper.Get("GOMAXPROCS")
+	fmt.Println("GOMAXPROCS:", val)
+
+	viper.BindEnv("NEW_VARIABLE")
+	val = viper.Get("NEW_VARIABLE")
+	if val == nil {
+		fmt.Println("NEW_VARIABLE not defined.")
+		return
+	}
 	fmt.Println(val)
 }
