2febd13aef6a7690a66cce14bcc75081c1fd1890
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2019-03-28 21:18:02 +0200

    Atomic package

--- ch10/atom.go
+++ ch10/atom.go
@@ -1,11 +1,39 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"sync"
 	"sync/atomic"
 )
 
-func main() {
+type atomicCounter struct {
+	val int64
+}
 
+func (c *atomicCounter) Value() int64 {
+	return atomic.LoadInt64(&c.val)
+}
+
+func main() {
+	minusX := flag.Int("x", 100, "Goroutines")
+	minusY := flag.Int("y", 200, "Value")
+	flag.Parse()
+	X := *minusX
+	Y := *minusY
+
+	var waitGroup sync.WaitGroup
+	counter := atomicCounter{}
+	for i := 0; i < X; i++ {
+		waitGroup.Add(1)
+		go func(no int) {
+			defer waitGroup.Done()
+			for i := 0; i < Y; i++ {
+				atomic.AddInt64(&counter.val, 1)
+			}
+		}(i)
+	}
+
+	waitGroup.Wait()
+	fmt.Println(counter.Value())
 }
