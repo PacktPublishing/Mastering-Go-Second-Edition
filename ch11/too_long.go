737976ded755a15c6a4a27169877f6a529cec31b
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2019-04-18 11:41:07 +0300

    go test -timeout examples

--- ch11/too_long.go
+++ ch11/too_long.go
@@ -1,12 +1,19 @@
 package main
 
 import (
-	"fmt"
 	"time"
 )
 
 func sleep_with_me() {
 	time.Sleep(5 * time.Second)
+}
+
+func get_one() int {
+	return 1
+}
+
+func get_two() int {
+	return 2
 }
 
 func main() {
--- ch11/too_long_test.go
+++ ch11/too_long_test.go
@@ -1,5 +1,29 @@
 package main
 
 import (
-	"fmt"
+	"testing"
 )
+
+func Test_test_one(t *testing.T) {
+	sleep_with_me()
+	value := get_one()
+	if value != 1 {
+		t.Errorf("Function returned %v", value)
+	}
+	sleep_with_me()
+}
+
+func Test_test_two(t *testing.T) {
+	sleep_with_me()
+	value := get_two()
+	if value != 2 {
+		t.Errorf("Function returned %v", value)
+	}
+}
+
+func Test_that_will_fail(t *testing.T) {
+	value := get_one()
+	if value != 2 {
+		t.Errorf("Function returned %v", value)
+	}
+}
