0775f156073409dd550f59ef909efabc0c402187
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2019-05-01 17:07:06 +0300

    Atomic in WWW

--- ch12/atomWWW.go
+++ ch12/atomWWW.go
@@ -10,13 +10,18 @@ import (
 var count int32
 
 func handleAll(w http.ResponseWriter, r *http.Request) {
-	temp := atomic.LoadInt32(&count)
 	atomic.AddInt32(&count, 1)
-	fmt.Println("count:", temp)
+}
+
+func getCounter(w http.ResponseWriter, r *http.Request) {
+	temp := atomic.LoadInt32(&count)
+	fmt.Println("Count:", temp)
+	fmt.Fprintf(w, "<h1 align=\"center\">%d</h1>", count)
 }
 
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU() - 1)
+	http.HandleFunc("/getCounter", getCounter)
 	http.HandleFunc("/", handleAll)
 	http.ListenAndServe(":8080", nil)
 }
