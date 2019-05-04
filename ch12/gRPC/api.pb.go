2693f7db27f7de5aaaee0db7f3b90378c3177b59
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2019-05-04 18:36:10 +0300

    Minor corrections + updates

--- ch12/gRPC/api.pb.go
+++ ch12/gRPC/api.pb.go
@@ -4,8 +4,12 @@
 package message_service
 
 import (
+	context "context"
 	fmt "fmt"
 	proto "github.com/golang/protobuf/proto"
+	grpc "google.golang.org/grpc"
+	codes "google.golang.org/grpc/codes"
+	status "google.golang.org/grpc/status"
 	math "math"
 )
 
@@ -133,4 +137,84 @@ var fileDescriptor_00212fb1f9d3bf1c = []byte{
 	0x59, 0x22, 0x24, 0xa1, 0x87, 0xe6, 0x3e, 0x3d, 0xa8, 0xe3, 0xa4, 0x24, 0xb1, 0xc8, 0x40, 0x6c,
 	0x4f, 0x62, 0x03, 0x7b, 0xcd, 0x18, 0x10, 0x00, 0x00, 0xff, 0xff, 0x63, 0x98, 0xe2, 0x43, 0xe7,
 	0x00, 0x00, 0x00,
+}
+
+// Reference imports to suppress errors if they are not otherwise used.
+var _ context.Context
+var _ grpc.ClientConn
+
+// This is a compile-time assertion to ensure that this generated file
+// is compatible with the grpc package it is being compiled against.
+const _ = grpc.SupportPackageIsVersion4
+
+// MessageServiceClient is the client API for MessageService service.
+//
+// For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
+type MessageServiceClient interface {
+	SayIt(ctx context.Context, in *Request, opts ...grpc.CallOption) (*Response, error)
+}
+
+type messageServiceClient struct {
+	cc *grpc.ClientConn
+}
+
+func NewMessageServiceClient(cc *grpc.ClientConn) MessageServiceClient {
+	return &messageServiceClient{cc}
+}
+
+func (c *messageServiceClient) SayIt(ctx context.Context, in *Request, opts ...grpc.CallOption) (*Response, error) {
+	out := new(Response)
+	err := c.cc.Invoke(ctx, "/message_service.MessageService/SayIt", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+// MessageServiceServer is the server API for MessageService service.
+type MessageServiceServer interface {
+	SayIt(context.Context, *Request) (*Response, error)
+}
+
+// UnimplementedMessageServiceServer can be embedded to have forward compatible implementations.
+type UnimplementedMessageServiceServer struct {
+}
+
+func (*UnimplementedMessageServiceServer) SayIt(ctx context.Context, req *Request) (*Response, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method SayIt not implemented")
+}
+
+func RegisterMessageServiceServer(s *grpc.Server, srv MessageServiceServer) {
+	s.RegisterService(&_MessageService_serviceDesc, srv)
+}
+
+func _MessageService_SayIt_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(Request)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(MessageServiceServer).SayIt(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/message_service.MessageService/SayIt",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(MessageServiceServer).SayIt(ctx, req.(*Request))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+var _MessageService_serviceDesc = grpc.ServiceDesc{
+	ServiceName: "message_service.MessageService",
+	HandlerType: (*MessageServiceServer)(nil),
+	Methods: []grpc.MethodDesc{
+		{
+			MethodName: "SayIt",
+			Handler:    _MessageService_SayIt_Handler,
+		},
+	},
+	Streams:  []grpc.StreamDesc{},
+	Metadata: "api.proto",
 }
--- ch12/gRPC/gServer.go
+++ ch12/gRPC/gServer.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/golang/protobuf/proto"
 	idf "github.com/mactsouk/protobuf"
+	"google.golang.org/grpc"
 	"net"
 )
 
