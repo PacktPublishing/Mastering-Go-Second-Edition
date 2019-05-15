5c7f1e29d6666ae97c2254c63b032dbd120c0f2a
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2019-05-15 23:04:34 +0300

    Minor changes

--- ch13/HTTPS/https.go
+++ ch13/HTTPS/https.go
@@ -1,40 +1,22 @@
 package main
 
 import (
-	"crypto/tls"
-	"crypto/x509"
 	"fmt"
-	"io/ioutil"
-	"log"
+	"io"
 	"net/http"
 )
 
-type handler struct {
-}
-
-func (h *handler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	w.Write([]byte("Hello there securely!\n"))
+func Default(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "This is an example server.\n")
+	io.WriteString(w, "This is an example server.\n")
 }
 
 func main() {
-	caCert, err := ioutil.ReadFile("client.crt")
+	http.HandleFunc("/", Default)
+	err := http.ListenAndServeTLS(":443", "server.crt", "server.key", nil)
+
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("ListenAndServe: ", err)
 		return
 	}
-
-	caCertPool := x509.NewCertPool()
-	caCertPool.AppendCertsFromPEM(caCert)
-	cfg := &tls.Config{
-		ClientAuth: tls.RequireAndVerifyClientCert,
-		ClientCAs:  caCertPool,
-	}
-
-	srv := &http.Server{
-		Addr:      ":8443",
-		Handler:   &handler{},
-		TLSConfig: cfg,
-	}
-
-	log.Fatal(srv.ListenAndServeTLS("server.crt", "server.key"))
 }
