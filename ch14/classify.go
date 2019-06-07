5e8391a45766f80e73a19aff6a57007743dca1ab
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2019-06-07 18:59:02 +0300

    kNN classification

--- ch14/classify.go
+++ ch14/classify.go
@@ -1,9 +1,46 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"github.com/sjwhitworth/golearn/base"
+	"github.com/sjwhitworth/golearn/evaluation"
+	"github.com/sjwhitworth/golearn/knn"
 )
 
 func main() {
+	flag.Parse()
+	if len(flag.Args()) == 0 {
+		fmt.Printf("usage: classify filename\n")
+		return
+	}
 
+	dataset := flagArgs()[0]
+	rawData, err := base.ParseCSVToInstances(dataset, false)
+	if err != nil {
+		panic(err)
+	}
+
+	// Print a pleasant summary of your data.
+	fmt.Println(rawData)
+
+	//Initialises a new KNN classifier
+	cls := knn.NewKnnClassifier("euclidean", "linear", 2)
+
+	//Do a training-test split
+	trainData, testData := base.InstancesTrainTestSplit(rawData, 0.50)
+	cls.Fit(trainData)
+
+	//Calculates the Euclidean distance and returns the most popular label
+	predictions, err := cls.Predict(testData)
+	if err != nil {
+		panic(err)
+	}
+
+	// Prints precision/recall metrics
+	confusionMat, err := evaluation.GetConfusionMatrix(testData, predictions)
+	if err != nil {
+		panic(fmt.Sprintf("Unable to get confusion matrix: %s", err.Error()))
+	}
+	fmt.Println(evaluation.GetSummary(confusionMat))
 }
