50cda40dc7213695f538b72dd3c319e8a08af51b
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2019-06-08 08:33:10 +0300

    Minor changes

--- ch14/classify.go
+++ ch14/classify.go
@@ -29,23 +29,15 @@ func main() {
 		return
 	}
 
-	// Print a pleasant summary of your data.
-	fmt.Println(rawData)
-
-	//Initialises a new KNN classifier
 	cls := knn.NewKnnClassifier("euclidean", "linear", k)
-
-	// Do a training-test split
 	trainData, testData := base.InstancesTrainTestSplit(rawData, 0.50)
 	cls.Fit(trainData)
 
-	//Calculates the Euclidean distance and returns the most popular label
 	predictions, err := cls.Predict(testData)
 	if err != nil {
 		panic(err)
 	}
 
-	// Prints precision/recall metrics
 	confusionMat, err := evaluation.GetConfusionMatrix(testData, predictions)
 	if err != nil {
 		panic(fmt.Sprintf("Unable to get confusion matrix: %s", err.Error()))
