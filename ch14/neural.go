9171e9cc52a2707e90ac28dddab0d3ccfa09ba62
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2019-06-15 08:57:34 +0300

    Simple Neural Network

--- ch14/neural.go
+++ ch14/neural.go
@@ -2,8 +2,32 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/goml/gobrain"
 )
 
 func main() {
+	seed := time.Now().Unix()
+	rand.Seed(seed)
 
+	patterns := [][][]float64{
+		{{0, 0, 0, 0}, {0}},
+		{{0, 1, 0, 1}, {1}},
+		{{1, 0, 1, 0}, {1}},
+		{{1, 1, 1, 1}, {1}},
+	}
+
+	ff := &gobrain.FeedForward{}
+	ff.Init(4, 2, 1)
+	ff.Train(patterns, 1000, 0.6, 0.4, false)
+
+	in := []float64{1, 1, 0, 1}
+	out := ff.Update(in)
+	fmt.Println(out)
+
+	in = []float64{0, 0, 0, 0}
+	out = ff.Update(in)
+	fmt.Println(out)
 }
